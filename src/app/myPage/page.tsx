7a9ada963f39ae0a645c6857faff6a6ab0aabd9e
amartyasarkar4 <sarkaramartya0@gmail.com>
2024-04-24 12:51:28 +0530

    clear for unble to preview in mobile

--- src/app/myPage/page.tsx
+++ src/app/myPage/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AnimatedTextWord from "@/components/reusable/textwriting";
 import MyPDFViewer from "@/react-pdf";
 
 import React from "react";
@@ -6,6 +7,17 @@ import React from "react";
 const MyPage = () => {
   return (
     <div>
+      <div className="flex justify-center w-full text-center">
+        <AnimatedTextWord
+          text="Hello,dear Sorry for unble to show resume preview in Mobile Device so PLEASE download It by Clicking Download Resume"
+          firstColor="text-cyan-700"
+          secondColor="bg-gradient-to-r from-sky-900 via-fuchsia-500 to-lime-800 text-transparent bg-clip-text"
+          secondColorAfterSpace={4}
+          breakAfterSpace={8}
+          textSize={"text-xl sm:text-3xl md:text-4xl lg:text-5xl"}
+        />
+      </div>
+
       <MyPDFViewer />
     </div>
   );
